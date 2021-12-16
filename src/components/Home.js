5f127bcce375c8048b64360a5cd7ba89849215d2
Milton Clark <kingcoder247@gmail.com>
2021-12-16 11:50:21 -0500

    Add - Navbar.js file to components folder, Homepage welcome message inside the Home.js file, route path inside switch for connections to other component pages from app

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+
+
+
+class Home extends Component{
+    render(){
+        return(
+            <div className="home-page">
+                <h1>
+                    Welcome to GhibliApp
+                </h1>
+  
+            </div>
+      );
+    };
+  };
+  
+export default Home;
